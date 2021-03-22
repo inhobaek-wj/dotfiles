847203efcb670a432babfeb1e120bd375b061ba6
inhobaek-wj <inhobaek.wj@gmail.com>
2021-03-22 15:05:07 +0900

    add vue mode hook

--- modules/lang-vue.el
+++ modules/lang-vue.el
@@ -1,10 +1,17 @@
 ;; requirement npm i -g vue-language-server
+(defun my-vue-mode-hook ()
+  (setq tab-width 2
+        js-indent-align-list-continuation nil
+        indent-tabs-mode nil))
+
 (use-package vue-mode
   :ensure t
   :mode "\\.vue\\'"
   :config
   (require 'lsp-mode)
-  (add-hook 'vue-mode-hook #'lsp))
+  (add-hook 'vue-mode-hook #'lsp)
+  (add-hook 'vue-mode-hook  'my-vue-mode-hook)
+  )
 
 ;; disable background color in vue mode.
 (add-hook 'mmm-mode-hook
