7182fb1caae00c4e9cddb611368d9dfc00d127e8
inhobaek-wj <inhobaek.wj@gmail.com>
2021-10-01 21:48:44 +0900

    feat: delete useless comment

--- init.el
+++ init.el
@@ -49,9 +49,6 @@
 ;;; packages
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;; (add-to-list 'package-archives '("melpa-milkbox" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
