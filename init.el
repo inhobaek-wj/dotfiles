897d473ee83a8b6fcbd76ce5d527e8192c2f5a71
jake <inhobaek.wj@gmail.com>
2023-01-17 16:42:25 +0900

    setenv for mac m1

--- init.el
+++ init.el
@@ -114,6 +114,8 @@
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
 
+(setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/12:/opt/homebrew/opt/libgccjit/lib/gcc/12:/opt/homebrew/opt/gcc/lib/gcc/12/gcc/aarch64-apple-darwin21/12")
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; custom global variables
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
