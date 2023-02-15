b407313c0712460de76216e4efe7b232461fda8b
jake <inhobaek.wj@gmail.com>
2023-02-15 13:08:41 +0900

    fix 'warning: -undefined dynamic_lookup may not work with chained fixups'

--- init.el
+++ init.el
@@ -125,6 +125,8 @@
 
 (setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/12:/opt/homebrew/opt/libgccjit/lib/gcc/12:/opt/homebrew/opt/gcc/lib/gcc/12/gcc/aarch64-apple-darwin21/12")
 
+(when (eq system-type 'darwin) (customize-set-variable 'native-comp-driver-options '("-Wl,-w")))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; custom global variables
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
